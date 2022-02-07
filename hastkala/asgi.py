08a1837d2687337235d1f0595f17cc5fd10b08b1
riyaz012 <riyaz13261@gmail.com>
2022-02-07 13:10:37 +0530

    django authentication

--- hastkala/asgi.py
+++ hastkala/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hasthkala project.
+ASGI config for hastkala project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hasthkala.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hastkala.settings')
 
 application = get_asgi_application()
--- hastkala/urls.py
+++ hastkala/urls.py
@@ -1,4 +1,4 @@
-"""hasthkala URL Configuration
+"""hastkala URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -24,4 +24,5 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
