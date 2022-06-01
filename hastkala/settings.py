ebd5abe410927cfe1baec1b056701eb9b5571fc7
riyaz <riyaz13261@gmail.com>
2022-06-01 09:08:05 +0530

    allowed host

--- hastkala/settings.py
+++ hastkala/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) #True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hastkala-env1.eba-fcapyfcs.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
